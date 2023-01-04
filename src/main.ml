f36a12aabd71b0caf2c7e10a2b88542f79726d47
coetaur0 <aurelien.coet19@gmail.com>
2023-01-04 16:30:37 +0100

    Fix main

--- src/main.ml
+++ src/main.ml
@@ -5,10 +5,8 @@ let () =
   try
     let stmts = Parser.parse source in
     let ty = Typechecker.infer stmts in
-    print_endline (Printer.string_of_type ty)
+    print_endline (Printer.type_repr ty)
   with
   | Parser.SyntaxError diagnostics ->
-    List.iter
-      (fun diagnostic -> print_endline (Printer.string_of_diagnostic diagnostic))
-      diagnostics
-  | Typechecker.TypeError diagnostic -> print_endline (Printer.string_of_diagnostic diagnostic)
+    List.iter (fun diagnostic -> print_endline (Printer.diagnostic_repr diagnostic)) diagnostics
+  | Typechecker.TypeError diagnostic -> print_endline (Printer.diagnostic_repr diagnostic)
